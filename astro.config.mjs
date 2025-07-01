5a65142c9cfc054bef0d8d7bebc90d557b996100
dw.office <dimaswahyu.official@gmail.com>
2025-07-01 15:02:21 +0700

    update baru

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
     output: 'static',
     trailingSlash: 'always',
-    site: 'https://devidev.io',
 
     // Single page, no prefetch needed
     prefetch: false,
